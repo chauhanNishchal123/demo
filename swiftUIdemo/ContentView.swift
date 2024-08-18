5158f7f1dbc5f0ad2e89b88dbb957381d7b97248
chauhanNishchal123 <nishchalchauhan424@gmail.com>
2024-08-18 14:22:48 +0530

    sdrf

--- swiftUIdemo/ContentView.swift
+++ swiftUIdemo/ContentView.swift
@@ -14,6 +14,9 @@ struct ContentView: View {
         VStack() {
             Text("Login").font(.largeTitle).padding()
             Text("for user get response").font(.subheadline)
+            Text("for user get response").font(.subheadline)
+            Text("for user get response").font(.subheadline)
+            Text("for user get response").font(.subheadline)
             TextField("username", text: $name).padding().cornerRadius(5.0).background(Color.gray)
             SecureField("password", text: $password).padding().cornerRadius(5.0).background(Color.brown)
             HStack {
